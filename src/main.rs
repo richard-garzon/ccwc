a1a5403e5e420214e797da697340d5918980e3d6
rg <garzon.r@gmail.com>
2024-06-18 00:54:27 -0400

    finished step one

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::{fs, io};
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "ccwc")]
+#[command(about = "wc impl", long_about = None)]
+struct Cli {
+    file_name: Option<String>,
+
+    #[arg(short = 'c', long = "")]
+    is_bytes: bool,
+}
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let file_name = cli.file_name;
+
+    let contents = match file_name {
+        Some(ref file_name) => fs::read_to_string(file_name).unwrap(),
+        None => io::read_to_string(io::stdin()).unwrap(),
+    };
+
+    println!("{} {}", contents.len(), file_name.unwrap());
 }
